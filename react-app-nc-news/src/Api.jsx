90f6295261e9635057d45f16f7d3964f95a198ff
bertf1966 <bertf1966@gmail.com>
2022-03-09 14:02:03 +0000

    pushing to switch branch

--- react-app-nc-news/src/Api.jsx
+++ react-app-nc-news/src/Api.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const newsArticles = axios.create({
-    baseUrl: "https://bertf-nc-news.herokuapp.com/api/"
+    baseURL: "https://bertf-nc-news.herokuapp.com/api/"
 })
--- react-app-nc-news/src/App.js
+++ react-app-nc-news/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
+import HomePage from './Components/Home-page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <HomePage />
     </div>
   );
 }
